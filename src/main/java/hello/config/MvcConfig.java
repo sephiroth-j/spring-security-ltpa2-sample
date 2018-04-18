c19d88e4206d8b4bbceafe44998a10f84bc6a588
sephiroth-j <sephiroth@sephiroth-j.de>
2018-04-18 21:04:53 +0200

    Update to Spring Boot 2.0.1 replace deprecated `WebMvcConfigurerAdapter` with `WebMvcConfigurer`

--- src/main/java/hello/config/MvcConfig.java
+++ src/main/java/hello/config/MvcConfig.java
@@ -17,10 +17,10 @@ package hello.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MvcConfig extends WebMvcConfigurerAdapter
+public class MvcConfig implements WebMvcConfigurer
 {
 
 	@Override

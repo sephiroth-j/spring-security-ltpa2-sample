538538f883f4e33b1565de6db1019be027f3d52d
sephiroth-j <sephiroth@sephiroth-j.de>
2019-05-04 21:12:55 +0200

    reuse `HttpHeaders.AUTHORIZATION`

--- src/test/java/hello/ApplicationTests.java
+++ src/test/java/hello/ApplicationTests.java
@@ -29,6 +29,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -61,7 +62,7 @@ public class ApplicationTests
 	@Test
 	public void accessSecuredResourceWithInvalidAuthenticationShouldBeForbidden() throws Exception
 	{
-		mockMvc.perform(get("/hello").header("Authorization", "sadfdas"))
+		mockMvc.perform(get("/hello").header(HttpHeaders.AUTHORIZATION, "sadfdas"))
 			.andExpect(status().isForbidden());
 	}
 
@@ -70,7 +71,7 @@ public class ApplicationTests
 	{
 		Ltpa2Token token = createTestToken();
 		
-		mockMvc.perform(get("/hello").header("Authorization", "LtpaToken2 ".concat(encryptToken(token))))
+		mockMvc.perform(get("/hello").header(HttpHeaders.AUTHORIZATION, "LtpaToken2 ".concat(encryptToken(token))))
 			.andExpect(status().isOk());
 	}
 
